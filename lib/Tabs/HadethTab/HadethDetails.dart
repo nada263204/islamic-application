5afab7edf932ecb5ffe9c712fc46b558615b929c
Your nada263204 <nadagharib2044@gmail.com>
2024-02-12 19:57:36 +0200

    finshing hadethtab

---
+++ lib/Tabs/HadethTab/HadethDetails.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:islami_application/Tabs/HadethTab/Hadeth.dart';
+import 'package:islami_application/Theme.dart';
+
+class HadethDetails extends StatelessWidget {
+  static const String routeName = 'hadeth-details';
+  
+
+  @override
+  Widget build(BuildContext context) {
+    Hadeth hadeth = ModalRoute.of(context)!.settings.arguments as Hadeth;
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(image:AssetImage('assets/images/default_bg.png'),
+          fit: BoxFit.fill)
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+          title: Text(hadeth.title),
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              color: AppTheme.White,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            margin: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height*0.05,
+              horizontal: MediaQuery.of(context).size.height*0.05,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ListView.builder(
+                itemCount: hadeth.content.length,
+                itemBuilder: (BuildContext context, int index) => Text(
+                  style: Theme.of(context).textTheme.titleLarge,
+                  hadeth.content[index],
+                  textDirection: TextDirection.rtl,),
+              ),
+            ),
+          ),
+          
+          
+        ))
+        ;
+  }
+  }
+  
